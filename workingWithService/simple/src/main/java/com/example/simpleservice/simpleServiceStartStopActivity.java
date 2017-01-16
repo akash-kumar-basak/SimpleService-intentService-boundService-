d80b5436a1755a80d42d71d6e7641057bd419db0
akash-kumar-basak <akash4116@gmail.com>
2017-01-15 23:41:50 -0800

    add a new MODULE for simple service work

---
+++ workingWithService/simple/src/main/java/com/example/simpleservice/simpleServiceStartStopActivity.java
@@ -0,0 +1,13 @@
+package com.example.simpleservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class simpleServiceStartStopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_service_start_stop);
+    }
+}
